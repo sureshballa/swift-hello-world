a95ba539438b1410c95f26ac2a6ecae9b0ff2934
Suresh Balla <suresh.balla@outlook.com>
2016-03-22 19:25:56 +0530

    Initial commit - hello world with story board and basic scene

---
+++ swifthelloworld/AddDetailsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AddDetailsViewController.swift
+//  swifthelloworld
+//
+//  Created by Suresh.Balla on 21/03/16.
+//  Copyright © 2016 sureshballa. All rights reserved.
+//
+
+import UIKit
+
+class AddDetailsViewController: UIViewController {
+
+    @IBOutlet var nameTextField: UITextField!
+    
+    @IBOutlet var detailsTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- swifthelloworld/ViewController.swift
+++ swifthelloworld/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let array = ["Item1", "Item2", "Item3"];
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.rowHeight = 70;
+        tableView.backgroundView = UIImageView(image: UIImage(named: "background"))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count;
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("customcell")! as UITableViewCell
+        
+        if(indexPath.item % 2 == 0){
+            cell.backgroundColor = UIColor.clearColor()
+        }
+        else{
+            cell.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.2)
+            cell.textLabel?.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.0)
+        }
+        
+        cell.textLabel?.text = array[indexPath.item]
+        
+        return cell
+    }
 
 }
 
